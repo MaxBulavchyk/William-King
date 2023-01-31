dc4864d068f3ea966eff36e7eccf669dc500ef17
MaxBulavchyk <maxblk555@gmail.com>
2023-01-31 12:00:36 +0100

    jc

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    items:2,
+    lazyLoad:true,
+    loop:true,
+    margin:10
+});
+
 function navToggle() {
     var navToggleNav = document.getElementById("nav");
     var html = document.getElementById("html");
@@ -6,4 +13,14 @@ function navToggle() {
     html.classList.toggle("_overflow");
     body.classList.toggle("_overflow");
 }
+
+function navClose() {
+    var navToggleNav = document.getElementById("nav");
+    navToggleNav.classList.remove("_active");
+    var html = document.getElementById("html");
+    html.classList.remove("_overflow");
+    var body = document.getElementById("body");
+    body.classList.remove("_overflow");
+  }
+  
 
